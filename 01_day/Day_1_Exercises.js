58435150809ce7559197d1ca6b504fc112935143
blank <108024965+MansoorAZ@users.noreply.github.com>
2023-09-04 15:48:44 -0400

    Update Day_1_Exercises.js

--- 01_day/Day_1_Exercises.js
+++ 01_day/Day_1_Exercises.js
@@ -2,9 +2,9 @@
 //comments can make code readable
   
 //2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30 days of JavaScript
 
 //3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
-//Welcome to 30 days of JavaScript
 /*
 Comments can make code readable,
 easy to reuse and informative
